048e4395a4c39023348d690c34832593546cd946
vxphat <vxphat.csdn@gmail.com>
2022-09-30 23:33:16 +0700

    fix

--- src/BaiTapLayoutComponent/Banner.jsx
+++ src/BaiTapLayoutComponent/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div>
         <header className="py-5">
-  <div className="container px-lg-5">
+  <div className="container">
     <div className="p-4 p-lg-5 bg-light rounded-3 text-center">
       <div className="m-4 m-lg-5">
         <h1 className="display-5 fw-bold">A warm welcome!</h1>
