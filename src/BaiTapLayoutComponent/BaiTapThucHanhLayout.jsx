f9cdfab4f7f9b2b68f1bd9a24c74b64818acfc82
vxphat <vxphat.csdn@gmail.com>
2022-09-30 21:49:01 +0700

    update source

---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+
+const BaiTapThucHanhLayout = () => {
+  return (
+    <div>
+        <header className='bg-dark'>
+        <div className="container">
+            <Header />
+        </div>
+        </header>
+        
+
+        
+    </div>
+  )
+}
+
+export default BaiTapThucHanhLayout
