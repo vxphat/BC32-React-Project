774f855d5c01717b9c01430db0eecdc2d2db67c5
vxphat <vxphat.csdn@gmail.com>
2022-10-01 12:08:47 +0700

    Fix icon

--- src/BaiTapLayoutComponent/Item.jsx
+++ src/BaiTapLayoutComponent/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Item = () => {
   return (
     <div className="container">
@@ -7,8 +8,9 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
-              <i class="fa-light fa-rectangle-history"></i>
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
+              <i class="bi bi-collection"></i>
+              
               </div>
               <h2 class="fs-4 fw-bold">Fresh new layout</h2>
               <p class="mb-0">
@@ -21,8 +23,8 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
-                <i class="bi bi-cloud-download"></i>
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
+              <i class="bi bi-cloud-download"></i>
               </div>
               <h2 class="fs-4 fw-bold">Free to download</h2>
               <p class="mb-0">
@@ -35,8 +37,8 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
-                <i class="bi bi-card-heading"></i>
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
+              <i class="bi bi-card-heading"></i>
               </div>
               <h2 class="fs-4 fw-bold">Jumbotron hero header</h2>
               <p class="mb-0">
@@ -48,7 +50,7 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
                 <i class="bi bi-bootstrap"></i>
               </div>
               <h2 class="fs-4 fw-bold">Feature boxes</h2>
@@ -61,7 +63,7 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
                 <i class="bi bi-code"></i>
               </div>
               <h2 class="fs-4 fw-bold">Simple clean code</h2>
@@ -75,7 +77,7 @@ const Item = () => {
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+              <div class="feature bg-primary bg-gradient text-white rounded-2 mb-4 mt-n4">
                 <i class="bi bi-patch-check"></i>
               </div>
               <h2 class="fs-4 fw-bold">A name you trust</h2>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <App />
+    
   
 );
 
